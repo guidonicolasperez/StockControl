19906ade5b6103940ba21153d4e90f6f70420adb
GuidoNP <GuidoNP@MAIN>
2021-02-03 15:37:34 -0300

    Inicio del proyecto. Se carga la documentacion y estructura dela BD

---
+++ Base de Datos/SQLQuery StockControl.sql
@@ -0,0 +1,38 @@
+-- Especifico la base de datos.
+USE StockControl
+
+drop table item
+drop table consumed
+
+-- Creo las tablas.
+CREATE TABLE item (
+	idItem int IDENTITY  NOT NULL,
+	itemDescription varchar(max) NOT NULL,
+	stock int NOT NULL,
+	stockAlert int,
+)
+
+CREATE TABLE consumed (
+	idConsumed int NOT NULL,
+	idItem int NOT NULL,
+	dateConsumed date NOT NULL,
+	quantityConsumed int NOT NULL,	
+)
+
+-- Asigno las PK
+ALTER TABLE item ADD PRIMARY KEY (idItem);
+ALTER TABLE consumed ADD PRIMARY KEY (idConsumed);
+
+-- Asigno las FK
+ALTER TABLE consumed ADD FOREIGN KEY (idItem) REFERENCES item (idItem);
+
+--DELETE FROM ITEM 
+
+INSERT INTO ITEM VALUES ('Martillo', 100, Null);
+INSERT INTO ITEM VALUES ('Destornillador', 20, 10);
+INSERT INTO ITEM VALUES ('Alicate', 30, 10);
+INSERT INTO ITEM VALUES ('Pinza', 40, 10);
+INSERT INTO ITEM VALUES ('Llave Inglesa', 50, 10);
+
+
+SELECT * FROM item
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # StockControl
 Software de control de stock
+
+[GitHub](https://github.com/guidonicolasperez/StockControl)
+[Trelo](https://trello.com/b/Z3paRmxN/stockcontrol) 
